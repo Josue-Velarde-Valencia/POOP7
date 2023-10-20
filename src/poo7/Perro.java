f682bae3c67d560c3d8a5b5fd168ecb63e4b1a9b
Josue-Velarde-Valencia <112038370+Josue-Velarde-Valencia@users.noreply.github.com>
2023-10-20 03:37:57 -0600

    Prácticas

---
+++ src/poo7/Perro.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package poo7;
+
+/**
+ *
+ * @author poo08alu24
+ */
+public class Perro extends AnimalTerrestre {
+    private String raza;
+
+    public Perro() {
+    }
+
+    public Perro(String raza, int numeroPatas, String nombre, String lugarOrigen, String color) {
+        super(numeroPatas, nombre, lugarOrigen, color);
+        this.raza = raza;
+    }
+
+    public String getRaza() {
+        return raza;
+    }
+
+    public void setRaza(String raza) {
+        this.raza = raza;
+    }
+
+    public void ladrar() {
+        System.out.println("Guau, guau");
+    }
+
+    @Override
+    public void comer() {
+        System.out.println("Estoy comiendo comida para perros");
+    }
+
+    @Override
+    public String toString() {
+        return "Perro{" + "raza=" + raza + super.toString() + '}';
+    }
+}
+
